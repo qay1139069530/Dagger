0ad22630bfd8604ae1ffc5b3f5a8acc20e448451
1139069530@qq.com <1139069530@qq.com>
2017-04-25 09:30:53 +0800

    update

---
+++ app/src/main/java/com/qay/qbase/dagger/util/ActivityUtil.java
@@ -0,0 +1,28 @@
+package com.qay.qbase.dagger.util;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import static dagger.internal.Preconditions.checkNotNull;
+
+/**
+ * Created by Qay on 2017/4/20.
+ */
+
+public class ActivityUtil {
+    /**
+     * The {@code fragment} is added to the container view with id {@code frameId}. The operation is
+     * performed by the {@code fragmentManager}.
+     *
+     */
+    public static void addFragmentToActivity (@NonNull FragmentManager fragmentManager,
+                                              @NonNull Fragment fragment, int frameId) {
+        checkNotNull(fragmentManager);
+        checkNotNull(fragment);
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(frameId, fragment);
+        transaction.commit();
+    }
+}
